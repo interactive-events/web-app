2d4cf0ad1b5eb8b91a8d126c752ca5854f571080
Sofie Nilsson <sofnii-9@student.ltu.se>
2014-10-01 16:05:00 +0200

    Skeleton up

---
+++ app/scripts/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name webAppApp
+ * @description
+ * # webAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('webAppApp', [
+    'ngAnimate',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name webAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the webAppApp
+ */
+angular.module('webAppApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
