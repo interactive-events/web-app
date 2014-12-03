b96f73892ce91708b71d4f801624d39a304a4661
Oskar Klang <oskar.klang@gmail.com>
2014-12-03 15:08:41 +0100

    pollresults added in poll template

--- app/scripts/controllers/neweventaddactivitymodal.js
+++ app/scripts/controllers/neweventaddactivitymodal.js
@@ -22,6 +22,11 @@ angular.module('ieventsWebApp')
                         answers: [
                             { id: 0 }
                         ]
+                    },
+                    eventFinished : false,
+                    pollResults : {
+                        numberOfVotes : 0,
+                        votes : []
                     }
                 },
                 state: 'planned'
@@ -41,6 +46,12 @@ angular.module('ieventsWebApp')
         };
 
         $scope.ok = function () {
+            for(var i=0; i<$scope.activity.customData.pollDescription.answers.length; i++) {
+                $scope.activity.customData.pollResults.votes.push({
+                    answerId: $scope.activity.customData.pollDescription.answers[i].id, 
+                    votes: 0
+                });
+            }
             $modalInstance.close($scope.activity);
         };
 
