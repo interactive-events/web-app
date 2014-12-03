7c805eed949bb7afb58db71e111f0d5c2294dd2d
Fredrik Lind <fredrik@editerat.se>
2014-12-03 15:16:31 +0100

    Registration

--- app/scripts/controllers/presentermodeviewactivity.js
+++ app/scripts/controllers/presentermodeviewactivity.js
@@ -10,23 +10,34 @@
 
 //TODO Generalize for use with different modules.
 angular.module('ieventsWebApp')
-    .controller('PresentermodeviewactivityCtrl', function ($scope, $state, $stateParams, Restangular) {
+    .controller('PresentermodeviewactivityCtrl', function ($scope, $state, $stateParams, Restangular, $interval) {
 
+        $scope.activityPromise = Restangular.one('events', $stateParams.eventId).one('activities', $stateParams.activityId).get();
+        $scope.activityPromise.then(function (data) {
+            $scope.activity = data.customData;
+            console.log($scope.activity);
+            function populateData() {
+                $scope.pollChart.series.push({
+                    type: 'pie',
+                    name: 'Votes',
+                    data: []
+                });
+                var numVotes = $scope.activity.pollResults.numberOfVotes;
 
-        /*$scope.activityPromise = Restangular.one('events/' + $stateParams.eventId + '/activities/' + $stateParams.activityId, $scope.eventId).get();
-         $scope.activityPromise.then(function (data) {
-         $scope.activity = data;
-         });*/
+                /* global angular: false */
+                angular.forEach($scope.activity.pollDescription.answers, function (value, index) {
+                    var dataItem = {name: value.answer};
+                    angular.forEach($scope.activity.pollResults.votes, function (value2, index2) {
+                        if (value.id.toString() === value2.answerId) {
+                            dataItem.y = parseFloat((value2.votes / numVotes) * 100);
+                            dataItem.name = dataItem.name + ': ' + value2.votes;
+                        }
+                    });
+                    $scope.pollChart.series[0].data[index] = dataItem;
+                });
+            }
 
-        function updatePercentages() {
-            $scope.pollChart.series[0].data[1][1] = cats / (dogs + sloths + cats);
-            $scope.pollChart.series[0].data[0][1] = dogs * 0.7 / (dogs + sloths + cats);
-            $scope.pollChart.series[0].data[2][1] = sloths * 1.3 / (dogs + sloths + cats);
-        }
-
-        $scope.activity = {
-            name: 'Poll',
-            customData: {
+            /*$scope.activity = {
                 hasVoted: true,
                 eventFinished: false,
                 pollDescription: {
@@ -54,77 +65,79 @@ angular.module('ieventsWebApp')
                         }
                     ]
                 }
+            };*/
+
+            if ($state.is('view-activity')) {
+                console.log('VOTER');
+            } else {
+                console.log('ADM');
             }
-        }
 
+            var socket = io.connect('http://interactive-events.elasticbeanstalk.com/events/' + $stateParams.eventId);
 
-        var cats = 1, dogs = 2, sloths = 4;
-
-        if ($state.is('view-activity')) {
-            console.log('VOTER');
-        } else {
-            console.log('ADM');
-        }
-
-        //var socket = io.connect('http://interactive-events.easticbeanstalk.com/events/' + $stateParams.eventId);
-
-        /*socket.on('vote', function (vote) {
-         if (vote.option === 0) {
-         cats++;
-         } else if (vote.option === 1) {
-         dogs++;
-         } else {
-         sloths++;
-         }
-         updatePercentages();
-         });*/
-
-        $scope.pollChart = {
-            title: {
-                text: $,
-                style: {'fontSize': '38px'}
-            },
-            series: [{
-                type: 'pie',
-                name: 'Browser share',
-                data: [
-                    ['Dogs', 25],
-                    ['Cats', 55],
-                    ['Sloth', 20]
-                ]
-            }],
-            options: {
-                chart: {
-                    spacingTop: 40,
-                    type: 'pie',
-                    options3d: {
-                        enabled: true,
-                        alpha: 45,
-                        beta: 0
+            socket.on('vote', function (vote) {
+                angular.forEach($scope.activity.pollResults.votes, function (value, index) {
+                    if (value.id.toString() === vote.answerId) {
+                        $scope.activity.pollResults.votes[index].votes++;
                     }
-                },
-                navigation: {
-                    buttonOptions: {
-                        enabled: false
+                });
+                populateData();
+            });
+
+            $interval(function () {
+                var vote = {answerId: 1};
+                angular.forEach($scope.activity.pollResults.votes, function (value, index) {
+                    if (value.answerId === vote.answerId) {
+                        console.log('Votes', $scope.activity.pollResults.votes[index].votes);
+                        $scope.activity.pollResults.votes[index].votes++;
                     }
+                });
+                populateData();
+
+            }, 200);
+
+            $scope.pollChart = {
+                title: {
+                    text: $scope.activity.pollDescription.question,
+                    style: {'fontSize': '38px'}
                 },
-                tooltip: {
-                    pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-                },
-                plotOptions: {
-                    pie: {
-                        showInLegend: true,
-                        allowPointSelect: true,
-                        cursor: 'pointer',
-                        depth: 35,
-                        dataLabels: {
+                series: [],
+                options: {
+                    chart: {
+                        spacingTop: 40,
+                        type: 'pie',
+                        options3d: {
                             enabled: true,
-                            format: '{point.name}',
-                            useHTML: true,
-                            style: {fontSize: '20px'}
+                            alpha: 45,
+                            beta: 0
+                        }
+                    },
+                    navigation: {
+                        buttonOptions: {
+                            enabled: false
+                        }
+                    },
+                    tooltip: {
+                        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+                    },
+                    plotOptions: {
+                        pie: {
+                            showInLegend: true,
+                            allowPointSelect: true,
+                            cursor: 'pointer',
+                            depth: 35,
+                            dataLabels: {
+                                enabled: true,
+                                format: '{point.name}',
+                                useHTML: true,
+                                style: {fontSize: '20px'}
+                            }
                         }
                     }
                 }
-            }
-        };
+            };
+
+            populateData();
+
+        });
     });
