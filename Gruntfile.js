ecde904b936fc02c40492613aad8c60b3259e613
sofienilsson <sofnii-9@student.ltu.se>
2015-01-19 16:26:07 +0100

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -210,8 +210,8 @@ module.exports = function (grunt) {
                 httpFontsPath: '/styles/fonts',
                 relativeAssets: false,
                 assetCacheBuster: false,
-                raw: 'Sass::Script::Number.precision = 10\n',
-                sourcemap: true
+                raw: 'Sass::Script::Number.precision = 10\n'
+                //sourcemap: true
             },
             dist: {
                 options: {
